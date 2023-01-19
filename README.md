7356426378661bfb1ae350eb9741e08764bb2d0a
cleyton21 <cfernando_21@hotmail.com>
2023-01-19 17:52:24 -0300

    crud de usuários de forma simples com laravel

---
+++ README.md
@@ -0,0 +1,7 @@
+#CRUD de Usuários com laravel
+
+1) Configurar a conexão com seu banco de dados no arquivo .env
+
+2) De maneira simples, rodar as migrations e seeds para gerar od dados.
+
+#php artisan migrate --seed
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [UsersController::class, 'index'])->name('user.index');
+Route::get('/usuario', [UsersController::class, 'create'])->name('user.create');
+Route::post('/usuario', [UsersController::class, 'store'])->name('user.store');
+Route::get('/usuario/{user}', [UsersController::class, 'show'])->name('user.show');
+Route::get('/usuario/editar/{user}', [UsersController::class, 'edit'])->name('user.edit');
+Route::put('/usuario/update/{user}', [UsersController::class, 'update'])->name('user.update');
+Route::delete('/usuario/delete/{user}', [UsersController::class, 'destroy'])->name('user.destroy');
+
